546d298ed6c971c93ada9285faf22d13a20a0947
carla-ng <carlitashata@gmail.com>
2022-02-22 23:32:31 +0100

    First commit.

---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <a href="/">Pokemon List</a>
+        <a href="/id">Pokemon por id</a>
+        <a href="/about">About</a>
+    </div>    
+</template>
+
+<style scoped>
+
+</style>
